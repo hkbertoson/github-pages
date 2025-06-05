f700bc8d6ef214ae4111a8dc6938060725378240
Marcus <mdavidson434@gmail.com>
2025-06-05 19:55:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hkbertoson.github.io',
+    site: 'https://mdav43.github.io',
     base: '/github-pages',
 });
